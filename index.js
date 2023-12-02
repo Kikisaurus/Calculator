e3201d8a86043260fb6b3b65e92846365cbf3d64
Keyshlianie Cosme <keyshlianiecosme@gmail.com>
2023-12-01 22:50:59 -0500

    Operators not working

---
+++ index.js
@@ -0,0 +1,70 @@
+const display = document.querySelector('#display');
+const clearButton = document.querySelector('#clear');
+const equalsButton = document.querySelector('#equals');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+
+let currentOperator = '';
+let firstNumber = '';
+let secondNumber = '';
+let result = '';
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (currentOperator === '') {
+            firstNumber += button.value;
+            display.innerText = firstNumber;
+        } else {
+            secondNumber += button.value;
+            display.innerText = secondNumber;
+        }
+    })
+})
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (firstNumber !== '' && secondNumber !== '') {
+            calculate();
+        }
+        currentOperator = button.value;
+        display.innerText = currentOperator;
+    })
+})
+
+equalsButton.addEventListener('click', () => {
+    if (firstNumber !== '' && secondNumber !== '') {
+        calculate();
+    }
+})
+
+clearButton.addEventListener('click', () => {
+    currentOperator = '';
+    firstNumber = '';
+    secondNumber = '';
+    result = '';
+    display.innerText = '0'
+})
+
+function calculate() {
+    switch (currentOperator) {
+        case '+':
+            result = parseFloat(firstNumber) + parseFloat(secondNumber);
+            break;
+        case '-':
+            result = parseFloat(firstNumber) - parseFloat(secondNumber);
+            break;
+        case '*':
+            result = parseFloat(firstNumber) * parseFloat(secondNumber);
+            break;
+        case '/':
+            result = parseFloat(firstNumber) / parseFloat(secondNumber);
+            break;
+            default:
+                return;
+    }
+
+    display.innerText = result;
+    firstNumber = result.toString();
+    secondNumber = '';
+    currentOperator = '';
+}
